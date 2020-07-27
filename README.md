c3dd258c5af981147c06b2d8494e4b2e499cbbf2
Быстрова Анна <a.bystrova@admitad.pro>
2020-07-27 23:49:25 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # devconnector
+
+lesson 7
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://admin:sly1MWehmb7OaxWZ@devconnector.ohpr1.mongodb.net/devconnector?retryWrites=true&w=majority'
+};
